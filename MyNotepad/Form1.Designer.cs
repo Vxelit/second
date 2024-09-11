9875360e5b652ba1f12ec6413dcd1929927c1f1a
Vxelit <channel050700@gmail.com>
2024-09-11 18:31:39 +0500

    Revert "зеленый цвет фона у формы,"
    This reverts commit c42710d8e8b4ca6fa8c1e173c0f357cd0f3b4ea7.

--- MyNotepad/Form1.Designer.cs
+++ MyNotepad/Form1.Designer.cs
@@ -28,14 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(343, 97);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Закрать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Блокнот
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Green;
+            this.BackColor = System.Drawing.Color.Orange;
             this.ClientSize = new System.Drawing.Size(793, 242);
+            this.Controls.Add(this.button1);
             this.Name = "Блокнот";
             this.Text = "Блокнот";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -44,6 +56,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
